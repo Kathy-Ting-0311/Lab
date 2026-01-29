310dac1d89092389034a4275b1d0519eee29663c
Kin Ting <jiantingh2@gmail.com>
2026-01-29 17:43:18 +0800

    Update Vocab2.java

--- Vocab2.java
+++ Vocab2.java
@@ -8,5 +8,18 @@
  * @author s235065
  */
 public class Vocab2 {
-    
+    public static void main(String[] args) {
+		VocabView view = new VocabView();
+		VocabModel model = new VocabModel();
+		VocabController control = new VocabController();
+
+		view.setController(control);
+		control.setModel(model);
+		control.setView(view);
+		model.setController(control);			
+	 
+		view.processCommand();
+		   
+	}
 }
+
