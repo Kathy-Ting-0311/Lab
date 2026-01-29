5f319dd0f12d4ac6bb25d2b987ca73cbb46c94a6
Kin Ting <jiantingh2@gmail.com>
2026-01-29 17:46:25 +0800

    Update VocabView.java

--- VocabView.java
+++ VocabView.java
@@ -8,5 +8,53 @@
  * @author s235065
  */
 public class VocabView {
+    private VocabController control;
+    private Scanner input = new Scanner(System.in);
     
+    public void setController(){
+    
+    }
+    
+    
+    public void processCommand(){
+        boolean end = false;
+        while (!end) {
+            System.out.println("Please enter your command");
+            System.out.print(">");
+            String command = input.next();
+            switch (command) {
+                case "add":
+                    doAdd();
+                    break;
+                case "lookup":  
+                    doLookup();
+                    break;
+                case "delete":
+                    doDelete();
+                    break;
+                case "exit":
+                    end = true;
+                    break;
+                default:
+                    System.out.println("Invalid command");
+            }
+        }
+    }
+    
+    public void doAdd(){
+    
+    }
+    
+    public void doDelete(){
+    
+    }
+    
+    public void doLookup(){
+    
+    }
+    
+    public void showMessage(String msg){
+    
+    }
 }
+
